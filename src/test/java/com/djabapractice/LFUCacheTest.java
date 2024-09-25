f431b2421f0dcdba9f8bc0c585a6c92c9fc50349
DimaGalchenko <galchenkodima31@gmail.com>
2024-09-25 13:20:31 +0300

    * update unit tests

--- src/test/java/com/djabapractice/LFUCacheTest.java
+++ src/test/java/com/djabapractice/LFUCacheTest.java
@@ -33,17 +33,15 @@ public class LFUCacheTest {
         lfuCache.put(2, "B");
         lfuCache.put(3, "C");
 
-        // All should be present
         assertEquals("A", lfuCache.get(1));
         assertEquals("B", lfuCache.get(2));
         assertEquals("C", lfuCache.get(3));
 
-        // Add new element, should evict least frequently used (key 1, 2, or 3)
         lfuCache.put(4, "D");
-        assertNull(lfuCache.get(1)); // Key 1 should be evicted since all have freq=1
-        assertEquals("B", lfuCache.get(2)); // Key 2 should be present
-        assertEquals("C", lfuCache.get(3)); // Key 3 should be present
-        assertEquals("D", lfuCache.get(4)); // Key 4 should be present
+        assertNull(lfuCache.get(1));
+        assertEquals("B", lfuCache.get(2));
+        assertEquals("C", lfuCache.get(3));
+        assertEquals("D", lfuCache.get(4));
     }
 
     @Test
@@ -52,38 +50,39 @@ public class LFUCacheTest {
         lfuCache.put(2, "B");
         lfuCache.put(3, "C");
 
-        lfuCache.get(1);  // Increase freq of key 1
-        lfuCache.get(1);  // Increase freq of key 1
+        lfuCache.get(1);
+        lfuCache.get(1);
 
-        // Add new element, should evict the least frequently used (key 2 or 3)
         lfuCache.put(4, "D");
-        assertNull(lfuCache.get(2)); // Key 2 should be evicted
-        assertEquals("A", lfuCache.get(1)); // Key 1 should still be present
-        assertEquals("C", lfuCache.get(3)); // Key 3 should still be present
-        assertEquals("D", lfuCache.get(4)); // Key 4 should be present
+        assertNull(lfuCache.get(2));
+        assertEquals("A", lfuCache.get(1));
+        assertEquals("C", lfuCache.get(3));
+        assertEquals("D", lfuCache.get(4));
     }
 
     @Test
     public void testFrequencyIncrease() {
         lfuCache.put(1, "A");
-        lfuCache.get(1);  // Freq of key 1 should now be 2
+        lfuCache.get(1);
 
         lfuCache.put(2, "B");
         lfuCache.put(3, "C");
-        lfuCache.put(4, "D");  // This should evict key 2 (lowest frequency)
+        lfuCache.put(4, "D");
 
-        assertNull(lfuCache.get(2)); // Key 2 should be evicted
-        assertEquals("A", lfuCache.get(1)); // Key 1 should still be present
-        assertEquals("C", lfuCache.get(3)); // Key 3 should still be present
-        assertEquals("D", lfuCache.get(4)); // Key 4 should be present
+        assertNull(lfuCache.get(2));
+        assertEquals("A", lfuCache.get(1));
+        assertEquals("C", lfuCache.get(3));
+        assertEquals("D", lfuCache.get(4));
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void testZeroCapacityCache() {
-        LFUCache<Integer, String> zeroCapacityCache = new LFUCache<>(0);
+        new LFUCache<>(0);
+    }
 
-        zeroCapacityCache.put(1, "A");
-        assertNull(zeroCapacityCache.get(1)); // Nothing should be stored
+    @Test(expected = IllegalArgumentException.class)
+    public void testNegativeCapacityCache() {
+        new LFUCache<>(-1);
     }
 
     @Test
@@ -91,9 +90,9 @@ public class LFUCacheTest {
         lfuCache.put(1, "A");
         lfuCache.put(2, "B");
 
-        lfuCache.put(1, "AA");  // Update key 1
-        assertEquals("AA", lfuCache.get(1)); // Value should be updated
-        assertEquals("B", lfuCache.get(2));  // Key 2 should still be present
+        lfuCache.put(1, "AA");
+        assertEquals("AA", lfuCache.get(1));
+        assertEquals("B", lfuCache.get(2));
     }
 
     @Test
@@ -102,16 +101,16 @@ public class LFUCacheTest {
         lfuCache.put(2, "B");
         lfuCache.put(3, "C");
 
-        lfuCache.get(1);  // Increase freq of key 1
+        lfuCache.get(1);
         lfuCache.get(1);
 
-        // Now all other keys have the same frequency, add another key
+
         lfuCache.put(4, "D");
 
-        assertNull(lfuCache.get(2)); // Key 2 should be evicted
-        assertEquals("A", lfuCache.get(1)); // Key 1 should be present
-        assertEquals("C", lfuCache.get(3)); // Key 3 should be present
-        assertEquals("D", lfuCache.get(4)); // Key 4 should be present
+        assertNull(lfuCache.get(2));
+        assertEquals("A", lfuCache.get(1));
+        assertEquals("C", lfuCache.get(3));
+        assertEquals("D", lfuCache.get(4));
     }
 
     @Test
@@ -119,9 +118,9 @@ public class LFUCacheTest {
         lfuCache.put(1, "A");
         lfuCache.put(2, "B");
         lfuCache.put(3, "C");
-        lfuCache.put(4, "D");  // Should evict least frequently used
+        lfuCache.put(4, "D");
 
-        assertNull(lfuCache.get(1)); // Key 1 should be evicted
+        assertNull(lfuCache.get(1));
         assertEquals("B", lfuCache.get(2));
         assertEquals("C", lfuCache.get(3));
         assertEquals("D", lfuCache.get(4));
@@ -134,14 +133,14 @@ public class LFUCacheTest {
         lfuCache.put(3, "C");
 
         lfuCache.get(1);
-        lfuCache.put(1, "AA");  // Update key 1
+        lfuCache.put(1, "AA");
 
-        lfuCache.put(4, "D");  // Should evict least frequently used
+        lfuCache.put(4, "D");
 
-        assertNull(lfuCache.get(2)); // Key 2 should be evicted
-        assertEquals("AA", lfuCache.get(1)); // Key 1 should still be present
-        assertEquals("C", lfuCache.get(3)); // Key 3 should still be present
-        assertEquals("D", lfuCache.get(4)); // Key 4 should be present
+        assertNull(lfuCache.get(2));
+        assertEquals("AA", lfuCache.get(1));
+        assertEquals("C", lfuCache.get(3));
+        assertEquals("D", lfuCache.get(4));
     }
 
     @Test
@@ -150,12 +149,12 @@ public class LFUCacheTest {
         lfuCache.put(2, "B");
         lfuCache.put(3, "C");
 
-        lfuCache.get(1);  // Freq of key 1 becomes 2
-        lfuCache.get(2);  // Freq of key 2 becomes 2
+        lfuCache.get(1);
+        lfuCache.get(2);
 
-        lfuCache.put(4, "D");  // Should evict key 3 (oldest among freq=1)
+        lfuCache.put(4, "D");
 
-        assertNull(lfuCache.get(3)); // Key 3 should be evicted
+        assertNull(lfuCache.get(3));
         assertEquals("A", lfuCache.get(1));
         assertEquals("B", lfuCache.get(2));
         assertEquals("D", lfuCache.get(4));
@@ -164,7 +163,7 @@ public class LFUCacheTest {
     @Test
     public void testGetOnEmptyCache() {
         LFUCache<Integer, String> emptyCache = new LFUCache<>(3);
-        assertNull(emptyCache.get(1)); // Empty cache should return null
+        assertNull(emptyCache.get(1));
     }
 
     @Test
@@ -174,7 +173,7 @@ public class LFUCacheTest {
         singleCapacityCache.put(1, "A");
         assertEquals("A", singleCapacityCache.get(1));
 
-        singleCapacityCache.put(2, "B");  // Should evict key 1
+        singleCapacityCache.put(2, "B");
         assertNull(singleCapacityCache.get(1));
         assertEquals("B", singleCapacityCache.get(2));
     }
@@ -184,40 +183,32 @@ public class LFUCacheTest {
         lfuCache.put(1, "A");
         lfuCache.put(2, "B");
 
-        lfuCache.get(1);  // Freq of key 1 increases
-        lfuCache.get(1);  // Freq of key 1 increases
+        lfuCache.get(1);
+        lfuCache.get(1);
 
         lfuCache.put(3, "C");
-        lfuCache.put(4, "D");  // Should evict key 2
+        lfuCache.put(4, "D");
 
-        assertNull(lfuCache.get(2)); // Key 2 should be evicted
-        assertEquals("A", lfuCache.get(1)); // Key 1 should still be present
-        assertEquals("C", lfuCache.get(3)); // Key 3 should still be present
-        assertEquals("D", lfuCache.get(4)); // Key 4 should be present
+        assertNull(lfuCache.get(2));
+        assertEquals("A", lfuCache.get(1));
+        assertEquals("C", lfuCache.get(3));
+        assertEquals("D", lfuCache.get(4));
     }
 
     @Test
     public void testKeyUpdateResetsFrequency() {
         lfuCache.put(1, "A");
-        lfuCache.get(1);  // Freq of key 1 increases
+        lfuCache.get(1);
 
-        lfuCache.put(1, "AA");  // Update key 1's value
+        lfuCache.put(1, "AA");
         lfuCache.put(2, "B");
         lfuCache.put(3, "C");
-        lfuCache.put(4, "D");  // Should evict key 2
+        lfuCache.put(4, "D");
 
-        assertNull(lfuCache.get(2)); // Key 2 should be evicted
-        assertEquals("AA", lfuCache.get(1)); // Updated key 1 should still be present
-        assertEquals("C", lfuCache.get(3)); // Key 3 should still be present
-        assertEquals("D", lfuCache.get(4)); // Key 4 should be present
-    }
-
-    @Test
-    public void testEdgeCaseForCapacityZero() {
-        LFUCache<Integer, String> zeroCapacityCache = new LFUCache<>(0);
-        zeroCapacityCache.put(1, "A");
-
-        assertNull(zeroCapacityCache.get(1)); // No items should be present
+        assertNull(lfuCache.get(2));
+        assertEquals("AA", lfuCache.get(1));
+        assertEquals("C", lfuCache.get(3));
+        assertEquals("D", lfuCache.get(4));
     }
 
     @Test
@@ -226,15 +217,15 @@ public class LFUCacheTest {
         lfuCache.put(2, "B");
         lfuCache.put(3, "C");
 
-        lfuCache.get(1);  // Freq of key 1 increases
-        lfuCache.get(1);  // Freq of key 1 increases
-        lfuCache.get(2);  // Freq of key 2 increases
+        lfuCache.get(1);
+        lfuCache.get(1);
+        lfuCache.get(2);
 
-        lfuCache.put(4, "D");  // Should evict key 3
+        lfuCache.put(4, "D");
 
-        assertNull(lfuCache.get(3)); // Key 3 should be evicted
-        assertEquals("A", lfuCache.get(1)); // Key 1 should be present
-        assertEquals("B", lfuCache.get(2)); // Key 2 should be present
-        assertEquals("D", lfuCache.get(4)); // Key 4 should be present
+        assertNull(lfuCache.get(3));
+        assertEquals("A", lfuCache.get(1));
+        assertEquals("B", lfuCache.get(2));
+        assertEquals("D", lfuCache.get(4));
     }
 }
